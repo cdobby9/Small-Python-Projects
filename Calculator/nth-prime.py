e1aab856417576d2b50af253a1c41a548cf9f32e
Charles Dobson <charlie2008dobson@hotmail.com>
2025-10-19 17:06:21 +0100

    Replaced factorials with prime detector

--- Calculator/nth-prime.py
+++ Calculator/nth-prime.py
@@ -1,12 +1,14 @@
 import math
 
-def willans_prime(n):
+def is_prime(j):
+    if j < 2: return 0
+    for k in range(2, int(j**0.5) + 1):
+        if j % k == 0: return 0
+    return 1
+
+def nth_prime(n):
     total = 0
     for i in range(1, 2**n + 1):
-        inner_sum = 0
-        for j in range(1, i + 1):
-            value = ((math.factorial(j - 1) + 1) / j)
-            cos_value = math.cos(math.pi * value)
-            inner_sum += math.floor(cos_value ** 2)
+        inner_sum = sum(is_prime(j) for j in range(1, i + 1))
         total += math.floor(n / (1 + inner_sum))
     return total + 1

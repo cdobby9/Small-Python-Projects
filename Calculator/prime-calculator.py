04bc0e40bee29db286b4409b42b0f0c57544bd61
Charles Dobson <charlie2008dobson@hotmail.com>
2025-10-20 18:13:20 +0100

    added ability to find the position of a prime

--- Calculator/prime-calculator.py
+++ Calculator/prime-calculator.py
@@ -17,5 +17,29 @@ def nth_prime(n):
             count_primes += 1
     return i
 
-n = int(input("Enter n: "))
-print("The", n, "th prime is", nth_prime(n))
+def prime_position(p):
+    if not is_prime(p):
+        return None
+    count_primes = 0
+    i = 1
+    while i <= p:
+        i += 1
+        if is_prime(i):
+            count_primes += 1
+        if i == p:
+            return count_primes
+
+mode = input("Find nth prime or find position of a prime? (enter 'n' or 'p'): ").strip().lower()
+
+if mode == 'n':
+    n = int(input("Enter n: "))
+    print("The", n, "th prime is:", nth_prime(n))
+elif mode == 'p':
+    p = int(input("Enter a prime number: "))
+    pos = prime_position(p)
+    if pos is None:
+        print(p, "is not prime")
+    else:
+        print(p, "is the", pos, "th prime.")
+else:
+    print("Invalid.")
